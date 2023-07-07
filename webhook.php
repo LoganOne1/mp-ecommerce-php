52579014852d5937e034be18b0bddab6a2120ffa
Brandon San Agustin <bsan5293@gmail.com>
2023-07-06 19:11:17 -0600

    Webhook testing

--- webhook.php
+++ webhook.php
@@ -28,4 +28,18 @@
           break;
   }
 
+  $json = file_get_contents('php://input');
+
+// Converts it into a PHP object
+$data = json_decode($json);
+
+$file = fopen("/tmp/mp_webhook_data.json", "a") or die("Cannot open file.");
+fwrite($file, $json);
+fclose($file);
+
+// render json response
+header('Content-Type: application/json');
+// echo json_encode($data);
+echo $json;
+
 ?>
